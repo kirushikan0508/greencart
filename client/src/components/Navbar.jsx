2ba425a02639af02de9c859c8c9fc9082ba8e231
kirushikan0508 <kirushikankiru01@gmail.com>
2025-06-05 01:00:06 +0530

    add the product detailspage and add cart icon for mobile screen in product details page there are some issues and that page is blanksoneed to fix the codeerror in the productDetails.jsx file

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { useAppContext } from '../context/AppContext'
 
 const Navbar = () => {
     const [open, setOpen] = React.useState(false)
-    const {user,setUser,setShowUserLogin,navigate, setSearchQuery, searchQuery} = useAppContext();
+    const {user,setUser,setShowUserLogin,navigate, setSearchQuery, searchQuery, getCartCount} = useAppContext();
 
     const logout = async() => {
         setUser(null);
@@ -41,7 +41,7 @@ const Navbar = () => {
 
                 <div onClick={()=>navigate('/cart')} className="relative cursor-pointer">
                     <img src={assets.nav_cart_icon} alt="cart" className='w-6 opacity-80' />
-                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount}</button>
                 </div>
 
                 {!user ? (<button onClick={()=> setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
@@ -58,11 +58,19 @@ const Navbar = () => {
                     </div>
                 )}
             </div>
+<div className='flex items-center gap-6 sm:hidden'>
 
-            <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
+    <div onClick={()=>navigate('/cart')} className="relative cursor-pointer">
+                    <img src={assets.nav_cart_icon} alt="cart" className='w-6 opacity-80' />
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount}</button>
+                </div>
+      <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="">
                 {/* Menu Icon SVG */}
                 <img src={assets.menu_icon} alt="menu"  />
             </button>
+
+</div>
+          
 
             {/* Mobile Menu */}
             { open && (
