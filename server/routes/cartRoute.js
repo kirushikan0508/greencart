c08e135a8c313a213c44a8946ba40c02b6796d46
kirushikan0508 <kirushikankiru01@gmail.com>
2025-06-08 16:31:47 +0530

    server side update

---
+++ server/routes/cartRoute.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose"
+import authUser from "../middlewares/authUser.js"
+import { updateCart } from "../controllers/cartControler.js"
+
+const cartRouter = mongoose.Router()
+
+cartRouter.post('.update', authUser, updateCart)
+export default cartRouter
